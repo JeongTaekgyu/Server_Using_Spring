f0c6be905a1d641009c1f4e637aff1c5934dc5cc
taekgyu <taekgyu0602@gmail.com>
2021-10-09 11:43:53 +0900

    post로 json데이터 받기

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,20 +1,28 @@
 package com.example.server.controller;
 
 import com.example.server.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    // 클라이언트가 보낸 내용을 경로에 따라 받음
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public User post(@RequestBody User user, @PathVariable int userId, @PathVariable String userName){
+        log.info("userId : {}, userName : {}", userId, userName);
+        log.info("client req : {},user");   // user의 toString이 {}안에 들어간다.
         return user;
     }
 }
